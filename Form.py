bc3ea5e3389c01678e460c890f7c14c20ca85245
Mahmoud Kamal Kandil <midokim1@gmail.com>
2022-04-03 04:04:07 +0200

    by samer

--- Form.py
+++ Form.py
@@ -1,18 +1,22 @@
 from tkinter import *
 from tkinter import ttk
 
+
 def Task1MainForm():
-    mForm1=Tk()
-    mForm1.geometry("700x700")
+    Form1 = Tk()
+    Form1.geometry("400x450")
+    Form1.resizable(False, False)
+    Form1.title("Perceptron learning algorithm")
     featuresList = ['F1 sepal length (cm)', 'F2 sepal width (cm)', 'F3 petal length (cm)', 'F4 petal width (cm)']
-    F1Label = Label(mForm1, text="Choose F1")
+    F1Label = Label(Form1, text="Choose Feature 1")
     F1Label.pack()
-    chosenF1 = ttk.Combobox(mForm1,values=featuresList)
+    chosenF1 = ttk.Combobox(Form1, values=featuresList)
     chosenF1.pack()
-    F2Label = Label(mForm1, text="Choose F2")
+    F2Label = Label(Form1, text="Choose Feature 2")
     F2Label.pack()
-    chosenF2 = ttk.Combobox(mForm1,values=[])
+    chosenF2 = ttk.Combobox(Form1, values=[])
     chosenF2.pack()
+
     def fillCB2(_):
         toRemove = chosenF1.get()
         chosenF2.set('')
@@ -21,35 +25,36 @@ def Task1MainForm():
         chosenF2['values'] = secList
     chosenF1.bind('<<ComboboxSelected>>', fillCB2)
 
-    Classes = ['C1','C2','C3']
-    c1Label = Label(mForm1, text="Choose Class 1")
+    Classes = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+    c1Label = Label(Form1, text="Choose Class 1")
     c1Label.pack()
-    chosenC1 = ttk.Combobox(mForm1, values=Classes)
+    chosenC1 = ttk.Combobox(Form1, values=Classes)
     chosenC1.pack()
-    c2Label = Label(mForm1, text="Choose Class 2")
+    c2Label = Label(Form1, text="Choose Class 2")
     c2Label.pack()
-    chosenC2 = ttk.Combobox(mForm1, values=[])
+    chosenC2 = ttk.Combobox(Form1, values=[])
     chosenC2.pack()
 
     def fillCB2Class(_):
         toRemove = chosenC1.get()
         chosenC2.set('')
-        secList = ['C1','C2','C3']
+        secList = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
         secList.remove(toRemove)
         chosenC2['values'] = secList
 
+    b = Button(Form1, text="Start", command=lambda: Form1.quit())
+    b.place(x=180, y=300)
     chosenC1.bind('<<ComboboxSelected>>', fillCB2Class)
-    etaLabel = Label(mForm1, text="Enter Learning rate(eta)")
-    epochsLabel = Label(mForm1, text="Enter number of epochs (m)")
-    etaTB = Entry(mForm1)
-    epochsTB = Entry(mForm1)
+    etaLabel = Label(Form1, text="Enter Learning rate(eta)")
+    epochsLabel = Label(Form1, text="Enter number of epochs (m)")
+    etaTB = Entry(Form1)
+    epochsTB = Entry(Form1)
     var1 = IntVar()
-    biasCheckbox = ttk.Checkbutton(mForm1, variable=var1, text='add bias?', onvalue=1, offvalue=0)
+    biasCheckbox = ttk.Checkbutton(Form1, variable=var1, text='add bias?', onvalue=1, offvalue=0)
     etaLabel.pack()
     etaTB.pack()
     epochsLabel.pack()
     epochsTB.pack()
     biasCheckbox.pack()
-
-
-    mForm1.mainloop()
+    Form1.mainloop()
+    return chosenF1.get(), chosenF2.get(), chosenC1.get(), chosenC2.get(), etaTB.get(), epochsTB.get()
--- Task1.py
+++ Task1.py
@@ -1,9 +1,40 @@
 from sklearn import datasets
-
+import  Perceptron
+import pandas as pd
 import Plotter
 import Form
+import Pre
 
-iris = datasets.load_iris()
+Iris = datasets.load_iris()
+Plotter.DrawIris(Iris)
 
-#Plotter.DrawIris(iris)
-Form.Task1MainForm()
+iris = pd.read_csv("Iris.csv")
+
+Feature1, Feature2, Class1, Class2, LearningRate, Epochs = Form.Task1MainForm()
+LearningRate = float(LearningRate)
+Epochs = int(Epochs)
+
+if Class1 == 'Iris-setosa' and Class2 == 'Iris-versicolor':
+    NewIris = Pre.Encode(iris, 1, 2)
+elif Class1 == 'Iris-versicolor' and Class2 == 'Iris-setosa':
+    NewIris = Pre.Encode(iris, 1, 2)
+elif Class1 == 'Iris-setosa' and Class2 == 'Iris-virginica':
+    NewIris = Pre.Encode(iris, 1, 3)
+elif Class1 == 'Iris-virginica' and Class2 == 'Iris-setosa':
+    NewIris = Pre.Encode(iris, 1, 3)
+else:
+    NewIris = Pre.Encode(iris, 2, 3)
+
+iris = Pre.Prepro(NewIris, Feature1, Feature2)
+
+iris = iris.astype(float)
+iris['Species'] = iris['Species'].astype(int)
+
+trainIris = iris[0:30]
+trainIris2 = iris[50:80]
+trainIris = trainIris.append(trainIris2)
+testIris = iris[30:50]
+testIris2 = iris[80:100]
+testIris = testIris.append(testIris2)
+
+Perceptron.train_weights(trainIris, LearningRate, Epochs)

2b4c2d7abf02c9efe2872d30598f4e7a7ddd920f
youssefgamal111 <youssefuture@outlook.com>
2022-04-05 06:47:02 +0200

    task2

---
+++ Task2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import Perceptron
+import pandas as pd
+import Plotter
+import Form
+import Pre
+import ConfusionMatrix
+import matplotlib.pyplot as plt
+
+import adaline
+import form2
+iris = pd.read_csv("Iris.csv")
+df = pd.DataFrame(iris)
+Plotter.DrawIris(iris)
+chosenF1, chosenF2, chosenC1, chosenC2, LearningRate, Epochs,bias,threshold=form2.Task2MainForm()
+LearningRate = float(LearningRate)
+Epochs = int(Epochs)
+threshold=float(threshold)
+data_train,data_test,data_trainNotShuffled = Pre.Encode(df, chosenC1, chosenC2,chosenF1, chosenF2, bias)
+Y_train=Pre.Encode2(data_train,chosenC1)
+Y_test=Pre.Encode2(data_test,chosenC1)
+weights = adaline.train_weights(data_train.iloc[:,0:3],Y_train, LearningRate, Epochs,threshold)
+accuracy,trueP,FalseP,FalseNeg,trueNeg = Perceptron.test_Data(data_test.iloc[:,0:3],Y_test,weights)
+
+print("Accuracy: ",accuracy)
+ConfusionMatrix.DrawMatrix(trueP,FalseP,FalseNeg,trueNeg)
---
+++ adaline.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+def predict(row, weights):
+    result = weights.T.dot(row)
+    return 1.0 if result >= 0.0 else -1.0
+
+
+def train_weights(trainX, TrainY, l_rate, n_epoch, threshold):
+    trainX = np.array(trainX)
+    weights = np.random.rand(trainX.shape[1])
+
+    for epoch in range(n_epoch):
+        for i, row in enumerate(trainX):
+            prediction = predict(row, weights)
+            error = TrainY[i] - prediction
+            weights = weights + l_rate * error * row
+        MSE = 0
+        for i, row in enumerate(trainX):
+            result = weights.T.dot(row)
+            error = TrainY[i] - result
+            MSE += float(((0.5) * error * error))
+        MSE *= float((1 / trainX.size))
+        if (MSE <= threshold):
+            break
+
+    return weights
+
+def test_Data(testX, testY, weights):
+    testX = np.array(testX)
+    weights = np.array(weights)
+    correct=0
+    trueP=0
+    trueNeg=0
+    FalseP=0
+    FalseNeg=0
+    for i, row in enumerate (testX):
+        predtected = predict(row,weights)
+        #truePositive/TrueNeg
+        if predtected == testY[i]:
+            correct+=1
+            if predtected==1:
+                trueP+=1
+            else:
+                trueNeg+=1
+        #FalsePositive/FalseNegative
+        if predtected != testY[i]:
+            if predtected==1:
+                FalseP+=1
+            else:
+                FalseNeg+=1
+
+    accuracy = (correct/len(testX))*100
+    return accuracy,trueP,FalseP,FalseNeg,trueNeg

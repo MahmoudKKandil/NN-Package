09fdf2da00cda48fca50653f3ff7a4a3ae3b2d29
youssefgamal111 <youssefuture@outlook.com>
2022-04-17 06:35:34 +0200

    little modify in forward func,create backward step func. and update weights func. .

--- BackPropagation.py
+++ BackPropagation.py
@@ -8,15 +8,68 @@ def ActivationFunction(net,Function):
   else:
       result = math.tanh(net)
   return result
+def BackActivationFunction(net,Function):
+  if Function=="SigmoidFunction":
+      result= net*(1-net)
 
-def forward(NumberOfLayers,row, weights,bias,ActivFunction):
+  else:
+      result = (1-net)*(1+net)
+  return result
+def forward(NumberOfLayers,row,weights,bias,ActivFunction):
+    arrayOfnet=[]
     for i in range(NumberOfLayers + 1):
      weight = np.array(weights[i])
      net = weight.dot(row)
      result=ActivationFunction(net,ActivFunction)
+     arrayOfnet.append(result)
      #7zawed el bias lel next iteration
      row=np.insert(result, 0, bias, axis=0)
-    return result
+    return arrayOfnet
+
+
+def back(arrayOfnetarrays,ActivFunction,weights,TrainY,bias,NumberOfLayers):
+    count=NumberOfLayers
+    arrOfSArrays=[]
+    newS=[]
+    while(count>-1):
+        nomOfNurons=len(arrayOfnetarrays[count])
+        if(arrOfSArrays!=[]):
+            lastArrIndex=len(arrOfSArrays)-1
+            oldS=arrOfSArrays[lastArrIndex]
+        for i in range(nomOfNurons):
+                if(count==NumberOfLayers):
+                        sNet=(TrainY[i]-arrayOfnetarrays[count][i])*BackActivationFunction(arrayOfnetarrays[count][i],ActivFunction)
+                else:
+                    for j in range(len(oldS)):
+                        sNet+=(oldS[j]*weights[count+1][j][i+bias])
+                    sNet*=BackActivationFunction(arrayOfnetarrays[count][i],ActivFunction)
+                newS.append(sNet)
+                sNet=0
+        count-=1
+        arrOfSArrays.append(newS);
+        newS=[]
+    return arrOfSArrays
+def updatewights(arrOfSArrays,arrayOfnetarrays,weights,l_rate,bias,NumberOfLayers,row):
+    for i in range(len(weights)):#layers
+        for j in range(len(weights[i])):#neron index
+            for k in range(len(weights[i][j])):#weight index eli daa5el flneron index
+                if i==0 :
+                    x=row[k]
+                else:
+                    if(bias==1 and k==0):
+                       x=1
+                    elif(bias==1):
+                        # i-1 3shan elnet bybtdi mn awl layer b3d elinput, fah index ellayer hyb2a 1 eli hwa i bs elindex flnet 0
+                        x = arrayOfnetarrays[i - 1][k - 1]
+                    else:
+                        x = arrayOfnetarrays[i - 1][k]
+
+                s = arrOfSArrays[i][j]
+
+
+                weights[i][j][k]= weights[i][j][k]+(l_rate*s*x)
+
+
 
 
 def create_array(m, n):
@@ -54,8 +107,9 @@ def Train(trainX, TrainY, l_rate, n_epoch,NumberOfLayers,neurons,numberOfFeature
     #bab3at el number of features 3shan de 3adad el neurons bta3et el input layer
     for epoch in range(n_epoch):
         for i, row in enumerate(trainX):
-            F = forward(NumberOfLayers,row, weights,bias,ActivFunction)
-            #kamel b2a  ya yong el backforward wel update
-            
+            arrayOfnetarrays = forward(NumberOfLayers,row, weights,bias,ActivFunction)
+            arrOfSArrays=back(arrayOfnetarrays,ActivFunction,weights,TrainY[i],bias,NumberOfLayers)
+            arrOfSArrays=np.flipud(arrOfSArrays)#ana 3mlt reverse llarray 3shan 25li ellocal gradient bta3 eloutput hwa eli fl25er w yb2a bnfs tarteb elarray eli feh elnet
+            updatewights(arrOfSArrays,arrayOfnetarrays,weights,l_rate,bias,NumberOfLayers,row)
 
     return weights
